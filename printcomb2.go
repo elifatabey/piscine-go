4081c0ea2756b68e7bcde96c7f814dc95740b584
atabeyelif <eliffatabey3@gmail.com>
2021-12-02 17:20:40 +0000

    important

--- printcomb2.go
+++ printcomb2.go
@@ -8,15 +8,28 @@ func PrintComb2() {
 	num := []rune("0123456789")
 	for i := 0; i <= 9; i++ {
 		for j := 0; j <= 9; j++ {
-			if i == 9 && j == 9 {
-				z01.PrintRune(num[i])
-				z01.PrintRune(num[j])
-				z01.PrintRune('\n')
-			} else {
-				z01.PrintRune(num[i])
-				z01.PrintRune(num[j])
-				z01.PrintRune(',')
-				z01.PrintRune(' ')
+			for k := 0; k <= 9; k++ {
+				for m := 0; m <= 9; m++ {
+					if i*10+j >= k*10+m {
+					} else {
+						if i == 9 && j == 8 && k == 9 && m == 9 {
+							z01.PrintRune(num[i])
+							z01.PrintRune(num[j])
+							z01.PrintRune(' ')
+							z01.PrintRune(num[k])
+							z01.PrintRune(num[m])
+							z01.PrintRune('\n')
+						} else {
+							z01.PrintRune(num[i])
+							z01.PrintRune(num[j])
+							z01.PrintRune(' ')
+							z01.PrintRune(num[k])
+							z01.PrintRune(num[m])
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
+						}
+					}
+				}
 			}
 		}
 	}
