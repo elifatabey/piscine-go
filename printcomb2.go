6ad104f9d7741b3dfdeb4f12ff5611547211ac11
atabeyelif <eliffatabey3@gmail.com>
2021-12-02 16:12:11 +0000

    quest 2 important

---
+++ printcomb2.go
@@ -0,0 +1,25 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func PrintComb2() {
+	num := []rune("0123456789")
+	for i := 0; i <= 9; i++ {
+
+		for j := 0; j <= 9; j++ {
+
+			if i == 9 && j == 9 {
+				z01.PrintRune(num[i])
+				z01.PrintRune(num[j])
+				z01.PrintRune('\n')
+			} else {
+				z01.PrintRune(num[i])
+				z01.PrintRune(num[j])
+				z01.PrintRune(',')
+				z01.PrintRune(' ')
+			}
+		}
+	}
+}
