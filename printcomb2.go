0a060d68e643bc7010fdd8f0b178618e2ecc4036
atabeyelif <eliffatabey3@gmail.com>
2021-12-02 23:59:15 +0000

    comb2 exercise different way

--- printcomb2.go
+++ printcomb2.go
@@ -5,29 +5,38 @@ import (
 )
 
 func PrintComb2() {
-	num := []rune("0123456789")
-	for i := 0; i <= 9; i++ {
-		for j := 0; j <= 9; j++ {
-			for k := 0; k <= 9; k++ {
-				for m := 0; m <= 9; m++ {
-					if i*10+j >= k*10+m {
-					} else {
-						if i == 9 && j == 8 && k == 9 && m == 9 {
-							z01.PrintRune(num[i])
-							z01.PrintRune(num[j])
+	for i := '0'; i <= '9'; i++ {
+		for j := '0'; j <= '9'; j++ {
+			for k := '0'; k <= '9'; k++ {
+				if i == k {
+					for m := j + 1; m <= '9'; m++ {
+						if i == '9' && j == '8' && k == '9' && m == '9' {
+							z01.PrintRune(i)
+							z01.PrintRune(j)
 							z01.PrintRune(' ')
-							z01.PrintRune(num[k])
-							z01.PrintRune(num[m])
+							z01.PrintRune(k)
+							z01.PrintRune(m)
 							z01.PrintRune('\n')
 						} else {
-							z01.PrintRune(num[i])
-							z01.PrintRune(num[j])
+							z01.PrintRune(i)
+							z01.PrintRune(j)
 							z01.PrintRune(' ')
-							z01.PrintRune(num[k])
-							z01.PrintRune(num[m])
+							z01.PrintRune(k)
+							z01.PrintRune(m)
 							z01.PrintRune(',')
 							z01.PrintRune(' ')
 						}
+					}
+				} else if i > k {
+				} else {
+					for m := '0'; m <= '9'; m++ {
+						z01.PrintRune(i)
+						z01.PrintRune(j)
+						z01.PrintRune(' ')
+						z01.PrintRune(k)
+						z01.PrintRune(m)
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
 					}
 				}
 			}
