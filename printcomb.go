1882d081c9a2fb559eea0581e71d3af8dcbe655b
atabeyelif <eliffatabey3@gmail.com>
2021-12-02 21:33:56 +0000

    exercise printcomb1

--- printcomb.go
+++ printcomb.go
@@ -7,7 +7,7 @@ import (
 func PrintComb() {
 	num := []rune("0123456789")
 	for i := 0; i <= 7; i++ {
-		for j := i + 1; j <= 8; j++ {
+		for j := i + 1; i <= 8; j++ {
 			for k := j + 1; k <= 9; k++ {
 				if i == 7 && j == 8 && k == 9 {
 					z01.PrintRune(num[i])
