7ea9ab5b9beccaa88751e7f1800143bc5a11c8a6
atabeyelif <eliffatabey3@gmail.com>
2021-12-06 19:23:12 +0000

    next prime

--- findnextprime.go
+++ findnextprime.go
@@ -6,13 +6,7 @@ func FindNextPrime(nb int) int {
 	} else {
 		for i := 2; i <= nb/2; i++ {
 			if nb%i == 0 {
-				for j := 1; j <= (nb+j)/2; j++ {
-					if (nb + j) == 0 {
-						return FindNextPrime(nb + j)
-					} else {
-						return nb + j
-					}
-				}
+				return FindNextPrime(nb + 1)
 			}
 		}
 	}
--- test/main.go
+++ test/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "piscine"
+import (
+	"fmt"
+	"piscine"
+)
 
 func main() {
-	piscine.PrintComb()
+	fmt.Println(piscine.FindNextPrime(93260))
+	fmt.Println(piscine.FindNextPrime(4))
 }
