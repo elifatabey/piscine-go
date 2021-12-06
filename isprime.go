38eb42993cd9245a70c9f7e7919a9171118450a0
atabeyelif <eliffatabey3@gmail.com>
2021-12-06 18:05:31 +0000

    test prime numbers

---
+++ isprime.go
@@ -0,0 +1,15 @@
+package piscine
+
+func IsPrime(nb int) bool {
+	if nb == 1 {
+		return false
+	}
+	for i := 2; i <= nb/2; i++ {
+		if nb%i == 0 {
+			return false
+		} else {
+			return true
+		}
+	}
+	return false
+}
