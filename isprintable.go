ef214e6fe724573abe8262ede91ed1a5905eb6f4
atabeyelif <eliffatabey3@gmail.com>
2021-12-07 16:18:17 +0000

    same with alpha

---
+++ isprintable.go
@@ -0,0 +1,35 @@
+package piscine
+
+func IsPrintable(s string) bool {
+	if s == "" {
+		return true
+	} else {
+		count := 0
+		for i := 'a'; i <= 'z'; i++ {
+			for k := 0; k < len([]rune(s)); k++ {
+				if rune(s[k]) == i {
+					count = count + 1
+				}
+			}
+		}
+		for i := 'A'; i <= 'Z'; i++ {
+			for k := 0; k < len([]rune(s)); k++ {
+				if rune(s[k]) == i {
+					count = count + 1
+				}
+			}
+		}
+		for i := '0'; i <= '9'; i++ {
+			for k := 0; k < len([]rune(s)); k++ {
+				if rune(s[k]) == i {
+					count = count + 1
+				}
+			}
+		}
+		if count == len([]rune(s)) {
+			return true
+		}
+	}
+	return false
+
+}
