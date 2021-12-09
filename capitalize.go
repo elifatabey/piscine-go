9f1cf758a42ffd8a5522ad0a3f50418eac98dfb6
atabeyelif <eliffatabey3@gmail.com>
2021-12-09 23:58:40 +0000

    exam pre

--- capitalize.go
+++ capitalize.go
@@ -4,7 +4,7 @@ func Capitalize(s string) string {
 	runes := []rune(s)
 	l := len(runes)
 	snew := []rune{}
-	for i := range s {
+	for i := range runes {
 		if i >= 65 && i <= 90 {
 			runes[i] = runes[i] - rune(32)
 		}
@@ -12,20 +12,19 @@ func Capitalize(s string) string {
 	if runes[0] >= 97 && runes[0] <= 122 {
 		runes[0] = runes[0] - rune(32)
 	}
-	for i := range s {
+	for i := range runes {
 		if runes[i] >= 32 && runes[i] <= 127 {
 			snew = append(snew, runes[i])
 		} else {
 			snew = append(snew, runes[i])
-			if i+1 < l-1 {
-				for j := i + 1; j < l-1; j++ {
-					if runes[j] == rune(32) {
-						snew = append(snew)
-					}
-					if runes[j] >= 97 && runes[j] <= 122 {
-						runes[j] = runes[j] - rune(32)
-						snew = append(snew, runes[j])
-					}
+			if i+1 > l-1 {
+			} else {
+
+				if runes[i+1] == rune(32) {
+					snew = append(snew)
+				}
+				if runes[i+1] >= 97 && runes[i+1] <= 122 {
+					runes[i+1] = runes[i+1] - rune(32)
 				}
 			}
 		}
