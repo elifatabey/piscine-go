45e27447ad956d05cbcc0fac20a66786aec0586e
atabeyelif <eliffatabey3@gmail.com>
2021-12-09 23:14:10 +0000

    exam pre

--- toupper.go
+++ toupper.go
@@ -2,9 +2,9 @@ package piscine
 
 func ToUpper(s string) string {
 	runes := []rune(s)
-	for i := range s {
-		if IsLower(string(runes[i])) {
-			runes[i] = runes[i] - rune(32)
+	for _, i := range runes {
+		if i >= 97 && i <= 122 {
+			i = i - 32
 		}
 	}
 	return string(runes)
