22f772336ef2219d2e4e4e6be554db396c34eee0
atabeyelif <eliffatabey3@gmail.com>
2021-12-09 17:38:34 +0000

    my first buble sorting

---
+++ sortparams/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	program := os.Args
+
+	for i := len(program); i > 0; i-- {
+		for j := 1; j < i; j++ {
+			if program[j-1] > program[j] {
+				middle := program[j]
+				program[j] = program[j-1]
+				program[j-1] = middle
+			}
+		}
+	}
+	for i := 1; i < len(program); i++ {
+		for _, r := range program[i] {
+			// if i > 1 {
+			z01.PrintRune(r)
+			// }
+		}
+		z01.PrintRune('\n')
+	}
+}
