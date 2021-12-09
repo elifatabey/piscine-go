583dfdf9306fe114157e918313fd328f04a9bb1b
atabeyelif <eliffatabey3@gmail.com>
2021-12-09 21:58:08 +0000

    try1

--- alphacount.go
+++ alphacount.go
@@ -3,7 +3,7 @@ package piscine
 func AlphaCount(s string) int {
 	count := 0
 	runeS := []rune(s)
-	for _,i := range runeS {
+	for _, i := range runeS {
 		if i >= 65 && i <= 90 || i >= 97 && i <= 122 {
 			count = count + 1
 		}
