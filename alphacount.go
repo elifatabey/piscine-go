fc3f9adf60e8049babdd9fc6067b79d4475388c3
atabeyelif <eliffatabey3@gmail.com>
2021-12-09 21:46:06 +0000

    try1

--- alphacount.go
+++ alphacount.go
@@ -1,12 +1,14 @@
 package piscine
 
-import ("fmt")
+import (
+	"fmt"
+)
 
 func AlphaCount(s string) int {
 	count := 0
-	for i:= range s {
-		if i >= 65 && i <=90 || i>=97 && i<=122 {
-			count = count +1
+	for i := range s {
+		if i >= 65 && i <= 90 || i >= 97 && i <= 122 {
+			count = count + 1
 		}
 	}
 	return count
