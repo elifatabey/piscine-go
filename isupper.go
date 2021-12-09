e6b6e3b8f398cb0ba225016bf46b1bd0f3a51c21
atabeyelif <eliffatabey3@gmail.com>
2021-12-09 23:02:55 +0000

    exam pre

--- isupper.go
+++ isupper.go
@@ -1,15 +1,15 @@
 package piscine
 
 func IsUpper(s string) bool {
-	count:= 0
+	count := 0
 	runes := []rune(s)
-	for _,i := range runes {
-		if i >= 65 && i<=90 {
+	for _, i := range runes {
+		if i >= 65 && i <= 90 {
 			count++
 		}
 	}
-	if count == len(runes){
+	if count == len(runes) {
 		return true
 	}
-return false
+	return false
 }
