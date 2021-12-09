3ffa851db21cd6540c3948a6656dd58c3660d01f
atabeyelif <eliffatabey3@gmail.com>
2021-12-09 16:36:35 +0000

    printparams

---
+++ printparams/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+func main() {
+	program := os.Args[1]
+	for _, r := range program {
+		// if i > 1 {
+		z01.PrintRune(r)
+		// }
+	}
+	z01.PrintRune('\n')
+}
