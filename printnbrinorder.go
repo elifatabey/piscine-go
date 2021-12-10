d3596a5f38161a7ffff697f51badb6ed2d06cd54
atabeyelif <eliffatabey3@gmail.com>
2021-12-10 09:23:49 +0000

    exam pre

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -5,12 +5,12 @@ import "github.com/01-edu/z01"
 func PrintNbrInOrder(n int) {
 	var nums []int
 	l := len(nums)
-	if n<0 {
-		return 
+	if n < 0 {
+		return
 	} else if n == 0 {
-		nums = append(nums,0)
+		nums = append(nums, 0)
 	}
-	for n>0 {
+	for n > 0 {
 		nums = append(nums, n%10)
 		n /= 10
 	}
@@ -18,11 +18,11 @@ func PrintNbrInOrder(n int) {
 	for i := 0; i < l-1; i++ {
 		for j := 0; j < l-1; j++ {
 			if nums[j] > nums[j+1] {
-				nums[j] , nums[j+1] = nums[j+1], nums[j]
+				nums[j], nums[j+1] = nums[j+1], nums[j]
 			}
 		}
 	}
-	for _,digit := range nums {
-		z01.PrintRune(rune(digit+48))
+	for _, digit := range nums {
+		z01.PrintRune(rune(digit + 48))
 	}
 }
