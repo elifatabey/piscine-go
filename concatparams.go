55bddf814a37105fcd0cef19870938038b5fcf74
atabeyelif <eliffatabey3@gmail.com>
2021-12-13 16:46:13 +0000

    concat strings

---
+++ concatparams.go
@@ -0,0 +1,15 @@
+package piscine
+
+func ConcatParams(args []string) string {
+	result := ""
+
+	for i := 0; i < len(args); i++ {
+		if i < len(args)-1 {
+			result = result + args[i] + "\n"
+		}
+		if i == len(args)-1 {
+			result = result + args[i]
+		}
+	}
+	return result
+}
