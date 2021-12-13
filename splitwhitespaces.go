5d60dc2fc41c2d9cfd5baadab92ac5d3c25e055e
atabeyelif <eliffatabey3@gmail.com>
2021-12-13 18:03:02 +0000

    split white space

---
+++ splitwhitespaces.go
@@ -0,0 +1,38 @@
+package piscine
+
+func SplitWhiteSpaces(s string) []string {
+	var final []string
+	countWords := 1
+	lenStr := 0
+	result := ""
+	for c := range s {
+		if isWhiteSpace(s[c]) {
+			countWords++
+		}
+		lenStr++
+	}
+	final = make([]string, countWords)
+	i := 0
+	for j, c := range s {
+		if j+1 == lenStr {
+			final[i] = result + string(s[j])
+		}
+		if isWhiteSpace(s[j]) {
+			if i <= countWords {
+				final[i] = result
+				i++
+				result = ""
+			}
+		} else {
+			result += string(c)
+		}
+	}
+	return final
+}
+
+func isWhiteSpace(r byte) bool {
+	if r == ' ' || r == '\n' || r == '\t' || r == 0 {
+		return true
+	}
+	return false
+}
