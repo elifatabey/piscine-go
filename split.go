646388f3da42c8d8205ad344df8c8eadc7df5344
atabeyelif <eliffatabey3@gmail.com>
2021-12-14 03:17:17 +0000

    split with string

--- split.go
+++ split.go
@@ -4,7 +4,7 @@ func Split(s, sep string) []string {
 	resp := []string{}
 	new := ""
 	for i := 0; i < len(s); i++ {
-		if Charset(s, sep, i) {
+		if setsep(s, sep, i) {
 			if new != "" {
 				resp = append(resp, new)
 				new = ""
@@ -20,7 +20,7 @@ func Split(s, sep string) []string {
 	return resp
 }
 
-func Charset(s, sep string, i int) bool {
+func setsep(s, sep string, i int) bool {
 	j := 0
 	for j < len(sep) && i < len(s) {
 		if s[i] != sep[j] {
