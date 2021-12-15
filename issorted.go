dd941f07e48bf264071367fb464026ef0ceeb423
atabeyelif <eliffatabey3@gmail.com>
2021-12-15 16:15:21 +0000

    outside f

--- issorted.go
+++ issorted.go
@@ -3,7 +3,7 @@ package piscine
 func IsSorted(f func(a, b int) int, a []int) bool {
 	count := true
 	for i := range a {
-		if a[i] != len(a)-1 {
+		if i != len(a)-1 {
 			if !(f(a[i], a[i+1]) > 0 || f(a[i], a[i+1]) < 0) {
 				count = false
 			}
