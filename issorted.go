3e2e4c7b4e89f28dfdda57325fe7c0530aee677e
atabeyelif <eliffatabey3@gmail.com>
2021-12-15 16:57:13 +0000

    outside f

--- issorted.go
+++ issorted.go
@@ -4,10 +4,10 @@ func IsSorted(f func(a, b int) int, a []int) bool {
 	var count []bool
 	for i := range a {
 		if i != len(a)-1 {
-			if !(f(a[i], a[i+1]) > 0 || f(a[i], a[i+1]) < 0) {
-				count = append(count, false)
-			} else {
+			if f(a[i], a[i+1]) == 1 || f(a[i], a[i+1]) == -1 {
 				count = append(count, true)
+			} else {
+				count = append(count, false)
 			}
 		}
 	}
