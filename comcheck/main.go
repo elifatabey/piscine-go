b64de19b495387ac9288804a5a75b0be7684ce27
atabeyelif <eliffatabey3@gmail.com>
2021-12-15 22:08:09 +0000

    hackaton 4

--- comcheck/main.go
+++ comcheck/main.go
@@ -7,11 +7,15 @@ import (
 
 func main() {
 	alertif := []string{"01", "galaxy", "galaxy 01"}
+	count := 0
 	for i := 0; i <= 2; i++ {
 		for _, str := range os.Args {
 			if str == alertif[i] {
-				fmt.Println("Alert!!!")
+				count++
 			}
 		}
+	}
+	if count > 1 {
+		fmt.Println("Alert!!!")
 	}
 }
