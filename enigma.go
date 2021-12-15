97fe3ad79583ee8b9e11e38bac6525b6d478a6c7
atabeyelif <eliffatabey3@gmail.com>
2021-12-15 22:20:07 +0000

    hackaton 5

---
+++ enigma.go
@@ -0,0 +1,11 @@
+package piscine
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	valuec := *******c
+	*******c = ***a
+	valued := ****d
+	****d = valuec
+	valb := *b
+	*b = valued
+	***a = valb
+}
