f306ad946b8521d9a109b417bf6365aff446a1e1
atabeyelif <eliffatabey3@gmail.com>
2021-12-16 02:06:29 +0000

    hackaton 8

---
+++ fixthemain/main.go
@@ -0,0 +1,50 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+}
+
+type Door struct {
+	state bool
+}
+
+const OPEN, CLOSE = false, true
+
+func OpenDoor(ptrDoor *Door) {
+	PrintStr("Door Opening...\n")
+	ptrDoor.state = OPEN
+}
+
+func CloseDoor(ptrDoor *Door) {
+	PrintStr("Door Closing...\n")
+	ptrDoor.state = CLOSE
+}
+
+func IsDoorOpen(ptrDoor Door) bool {
+	PrintStr("is the Door opened ?\n")
+	return ptrDoor.state
+}
+
+func IsDoorClose(ptrDoor Door) bool {
+	PrintStr("is the Door closed ?\n")
+	return ptrDoor.state
+}
+
+func main() {
+	door := Door{}
+
+	OpenDoor(&door)
+	if IsDoorClose(door) {
+		OpenDoor(&door)
+	}
+	if IsDoorOpen(door) {
+		CloseDoor(&door)
+	}
+	if door.state == OPEN {
+		CloseDoor(&door)
+	}
+}
