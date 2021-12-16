393fb23c24b9eb090c49443852e77badb361bf07
atabeyelif <eliffatabey3@gmail.com>
2021-12-16 23:41:18 +0000

    hackhaton 13

--- max.go
+++ max.go
@@ -6,8 +6,10 @@ func Max(a []int) int {
 	} else {
 		var newa []int = a
 		for i := 0; i < len(a)-1; i++ {
-			if newa[i] < newa[i+1] {
-				newa[i], newa[i+1] = newa[i+1], newa[i]
+			for j := 0; j < len(a)-1; j++ {
+				if newa[j] < newa[j+1] {
+					newa[j], newa[j+1] = newa[j+1], newa[j]
+				}
 			}
 		}
 		return newa[0]
